99ca89de334d2e8290e04995f31e7f1c8ae343c6
Youval VANLAER <gradapin@gmail.com>
2018-02-13 20:35:21 +0100

    fifth commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var homepage = require('./routes/index');
+var webhook = require('./routes/webhook');
 
 var app = express();
 
@@ -22,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', homepage);
-
+app.use('/webhook/', webhook);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -42,3 +43,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
